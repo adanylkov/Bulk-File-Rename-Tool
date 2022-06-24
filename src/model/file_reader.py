20c89a8677d53a78345ee08514dddaf6da0e7758
Andrii Danylkov <danilkovxp@gmail.com>
2022-06-24 13:48:45 +0200

    Use current directory if not specified

--- src/model/file_reader.py
+++ src/model/file_reader.py
@@ -1,16 +1,18 @@
 import os
 
 
-def list_files(path : str):
-    return os.listdir(path)
+def list_files(path : str = os.curdir):
+    return list(filter(lambda file: os.path.isfile(file), os.listdir(path)))
 
 
-def rename_file(dir, src, dest):
+def rename_file(src, dest, dir = os.curdir):
     src = os.path.join(dir, src)
     dest = os.path.join(dir, dest)
     os.rename(src, dest)
 
 
-def create_file(dir, name):
+def create_file(name, dir = os.curdir):
     name = os.path.join(dir, name)
     open(name, "w").close()
+
+
